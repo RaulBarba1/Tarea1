ecca8e0028072771e8f05aac75c489451994efea
raulbarba <luar_barba@hotmail.com>
2019-08-23 16:36:24 -0600

    multiplicacion

---
+++ multiplicacion.js
@@ -0,0 +1,6 @@
+var a = 12;
+var b = 17;
+showproduct(a, b);
+function showproduct(first, second) {
+    console.log("The product is: " + first + second);
+}
---
+++ multiplicacion.ts
@@ -0,0 +1,9 @@
+let aa: number = 12; 
+let ba: number = 17;
+ 
+function showProduct(first: number, second: number): void {    
+ console.log("The product is: " +  first * second); 
+
+}
+ 
+showProduct(aa, ba);
