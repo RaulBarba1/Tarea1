828eb9c65e2f7424df7f5c55d06e5b5f90991cfb
raulbarba <luar_barba@hotmail.com>
2019-08-23 17:32:15 -0600

    cadena plantilla

---
+++ cadena.js
@@ -0,0 +1,8 @@
+var e = "Building";
+var f = 300;
+var sentence = "The " + e + " in front of my office us " + f + " feet tall ";
+var g = "Building";
+var h = 300;
+var sentence2 = "the " + g + " in front my office is " + h + " feet tall  ";
+console.log(sentence);
+console.log(sentence2);
---
+++ cadena.ts
@@ -0,0 +1,10 @@
+var e = "Building";
+var f = 300;
+var sentence = "The "+e+" in front of my office us "+ f +" feet tall ";
+var g = "Building";
+var h = 300;
+var sentence2 =  `the ${g} in front my house is ${h} feet tall  `;
+
+
+console.log(sentence);
+console.log(sentence2);
