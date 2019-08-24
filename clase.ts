d04ab01370a18a50caa44fa43e6843346914eeaa
raulbarba <luar_barba@hotmail.com>
2019-08-23 20:07:19 -0600

    modificador protegido-clase

--- clase.ts
+++ clase.ts
@@ -1,27 +1,31 @@
-class Person {  
-    name: string; 
-    constructor(theName: string) {  
-        this.name = theName;
-    }
-    introduceSelf() {  
-        console.log("Hi, I am " + this.name + "!");
-    }
+class Person {
+    private name: string;
+    protected age: number;
+    protected constructor(theName: string, theAge: number) {   
+    this.name = theName;       
+    this.age = theAge; 
     }
 
-    class Friend extends Person {
-    yearsKnown: number;    
-    constructor(name: string, yearsKnown: number) {     
-    super(name);        
-    this.yearsKnown = yearsKnown; 
-    }  
-    timeKnown() {   
-    console.log("We have been friends for " + this.yearsKnown + " years.") 
-   
-}
+    introduceSelf() {     
+    console.log("Hi, I am " + this.name + "!");   
+      }
     }
- 
-let friendA = new Friend("Barba", 8);
- 
-friendA.introduceSelf();
-friendA.timeKnown();
- 
+
+    class Friend extends Person { 
+    yearsKnown: number;    
+    constructor(name: string, age: number, yearsKnown: number) { 
+    super(name, age);        
+    this.yearsKnown = yearsKnown;    
+    } 
+    timeKnown() {       
+    console.log("We have been friends for " + this.yearsKnown + " years.")
+    }
+    friendSince() {      
+    let firstAge = this.age - this.yearsKnown;       
+     console.log(`We have been friends since I was ${firstAge} years old.`)
+        }
+     }
+     
+
+     let friendA = new Friend("William", 19, 8);
+     friendA.friendSince();
